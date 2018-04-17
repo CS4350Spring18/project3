bc20ae922ed0c2b151432b1827639e40fc312ca0
Sherman <stt19@txstate.edu>
2018-04-17 18:24:26 -0500

    adding mycd

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 project=mysh
-objects=mysh.o
+objects=mysh.o mycd.o
 cc=gcc
 cflags=-Wall -std=c99 -g -pedantic
 
---
+++ mycd.h
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool mycd(char *filename);
